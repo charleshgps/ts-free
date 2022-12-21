4edd9db09ceba794df5055122715a4555a48c791
Charles Silva <42080053+charleshgps@users.noreply.github.com>
2022-12-20 21:16:41 -0300

    remove coments  js

--- dist/js/index.js
+++ dist/js/index.js
@@ -2,28 +2,21 @@
 let x = 10;
 x = 12;
 console.log(x);
-//inferenai x annotation
 const y = 12;
-//tipos
 let firstName = "Charles";
 let age = 29;
 const isAdmin = true;
-//String != string
-//Object
 const myNumbers = [1, 2, 3];
 console.log(myNumbers);
 console.log(myNumbers.length);
 console.log(firstName.toUpperCase());
-//tuplas
 let myTuple;
 myTuple = [5, "Teste", ["a", "b"]];
-//Objects literals -> {prop: value}
 const user = {
     name: "Charles",
     age: 29,
 };
 console.log(user.name, user.age);
-//Any
 let a = 0;
 a = "teste";
 console.log(a);
@@ -31,14 +24,11 @@ a = true;
 console.log(a);
 a = [];
 console.log(a);
-//Union Types
 let id = "10";
 id = 200;
 const userId = 10;
 const productId = "00001";
 const shirId = 123;
-//Enum
-//Tamanho de roupas
 var Size;
 (function (Size) {
     Size["P"] = "Pequeno";
@@ -49,12 +39,9 @@ const camisa = {
     name: "Camisa",
     size: Size.G,
 };
-//Literal types
 let teste;
-//teste: "outrovalor";
 teste = "autenticado";
 teste = null;
-//Funções
 function sum(a, b) {
     return a + b;
 }
